51bf5a42d04f4785ba9e1f004dc96256d6394408
kumawabh <kumawabh@WL350183>
2020-07-17 16:43:39 +0530

    minor change

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -14,7 +13,6 @@ module.exports = {
     ],
   },
   plugins: [
-      new CleanWebpackPlugin()
   ],
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
