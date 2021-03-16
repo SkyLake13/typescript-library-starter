6e5f47387e94d2490d8266cb2ed9693825b5aa4a
kumawabh <kumawabh@WL350183>
2021-03-16 11:57:55 +0530

    react library starter kit

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function HelloWorld(): JSX.Element {
+    return (
+        <div>
+            <h3>Hello world from react component library</h3>
+        </div>
+    );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './starter';
+export * from './components/HelloWorld';
